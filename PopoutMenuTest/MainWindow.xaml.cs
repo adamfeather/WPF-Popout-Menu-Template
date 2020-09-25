ed0df059fdc16c4630eb3e65ba3c3e723be68e09
Adam Feather <FEATHEA@domesticandgeneral.com>
2020-09-25 11:48:20 +0100

    initial commit

---
+++ PopoutMenuTest/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Animation;
+
+namespace PopoutMenuTest
+{
+    public partial class MainWindow : Window
+    {
+        private bool _isMenuExpanded = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ExpandCollapseMenuIcon_Click(object sender, MouseButtonEventArgs e)
+        {
+            var storyboardName = _isMenuExpanded ? "CollapseMenuStoryboard" : "ExpandMenuStoryboard";
+
+            var storyboard = (Storyboard)FindResource(storyboardName);
+
+            storyboard.Begin(this);
+
+            _isMenuExpanded = !_isMenuExpanded;
+
+        }
+
+        private void CloseWindowIcon_Click(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void TitleBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+    }
+}
